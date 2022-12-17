480cb667c4b115ba7637ada653136b63874ad16c
Raef <bakleh.raef@gmail.com>
2022-12-17 04:26:05 +0100

    changes

---
+++ build/precache-manifest.6d2c28b5d25862bca18317b3731aaa4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad98ad05173b8b986304",
+    "url": "/static/css/main.5388d3ed.chunk.css"
+  },
+  {
+    "revision": "ad98ad05173b8b986304",
+    "url": "/static/js/main.e043da5f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "74275cf8e748fb78d575",
+    "url": "/static/js/2.93261fc4.chunk.js"
+  },
+  {
+    "revision": "e4f2c5c3e3e62b21b63e9e59a3f00dd2",
+    "url": "/index.html"
+  }
+];
